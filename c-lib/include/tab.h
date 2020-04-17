7d61b2be7d998a03a475b5f4f852d4db424e4e33
Benjamin Reigner <benjamin.reigner@epitech.eu>
2020-04-17 19:44:42 +0200

    [EDIT] tab.h to remove deleted functions

--- c-lib/include/tab.h
+++ c-lib/include/tab.h
@@ -11,8 +11,6 @@
 int count_line_len(const char *str);
 int count_line_nb(const char *str);
 char **fill_tab(const char *str);
-char **my_str_to_word_array(char const *str);
-char *my_strtok(char *str, char const *delim);
 int my_tablen(char **tab);
 char **str_to_arr(char *str, char *delim);
 
