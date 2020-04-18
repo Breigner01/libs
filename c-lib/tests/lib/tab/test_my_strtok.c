570302e8936cfa61e6387da69286f014c4275d00
Benjamin Reigner <benjamin.reigner@epitech.eu>
2020-04-18 23:51:25 +0200

    [EDIT] my_strtok tests variables names

--- c-lib/tests/lib/tab/test_my_strtok.c
+++ c-lib/tests/lib/tab/test_my_strtok.c
@@ -13,70 +13,70 @@ TestSuite(my_strtok, .timeout=0.1);
 Test(my_strtok, classical_string)
 {
     char str[] = "Hello\nCoucou\nTest\nEpitech\n";
-    char *str_1 = strtok(str, "\n");
-    char *str_2 = my_strtok(str, "\n");
+    char *tok_1 = strtok(str, "\n");
+    char *tok_2 = my_strtok(str, "\n");
 
-    while (str_1 && str_2) {
-        cr_assert_str_eq(str_2, str_1);
-        str_1 = strtok(NULL, "\n");
-        str_2 = my_strtok(NULL, "\n");
+    while (tok_1 && tok_2) {
+        cr_assert_str_eq(tok_2, tok_1);
+        tok_1 = strtok(NULL, "\n");
+        tok_2 = my_strtok(NULL, "\n");
     }
-    cr_assert_null(str_1);
-    cr_assert_null(str_2);
+    cr_assert_null(tok_1);
+    cr_assert_null(tok_2);
 }
 
 Test(my_strtok, string_without_delim)
 {
     char str[] = "Hello Coucou Test Epitech";
-    char *str_1 = strtok(str, "\n");
-    char *str_2 = my_strtok(str, "\n");
+    char *tok_1 = strtok(str, "\n");
+    char *tok_2 = my_strtok(str, "\n");
 
-    cr_assert_str_eq(str_2, str_1);
+    cr_assert_str_eq(tok_2, tok_1);
 }
 
 Test(my_strtok, string_with_mult_delim)
 {
     char str[] = "Hello\n\n\n\nCoucou\n\n\nTest\n\nEpitech\n\n\n\n\n\n\n\n\n";
-    char *str_1 = strtok(str, "\n");
-    char *str_2 = my_strtok(str, "\n");
+    char *tok_1 = strtok(str, "\n");
+    char *tok_2 = my_strtok(str, "\n");
 
-    while (str_1 && str_2) {
-        cr_assert_str_eq(str_2, str_1);
-        str_1 = strtok(NULL, "\n");
-        str_2 = my_strtok(NULL, "\n");
+    while (tok_1 && tok_2) {
+        cr_assert_str_eq(tok_2, tok_1);
+        tok_1 = strtok(NULL, "\n");
+        tok_2 = my_strtok(NULL, "\n");
     }
-    cr_assert_null(str_1);
-    cr_assert_null(str_2);
+    cr_assert_null(tok_1);
+    cr_assert_null(tok_2);
 }
 
 Test(my_strtok, string_with_diff_delim)
 {
     char str[] = "Hello Coucou!Test:Epitech";
-    char *str_1 = strtok(str, " !:");
-    char *str_2 = my_strtok(str, " !:");
+    char *tok_1 = strtok(str, " !:");
+    char *tok_2 = my_strtok(str, " !:");
 
-     while (str_1 && str_2) {
-        cr_assert_str_eq(str_2, str_1);
-        str_1 = strtok(NULL, " !:");
-        str_2 = my_strtok(NULL, " !:");
+     while (tok_1 && tok_2) {
+        cr_assert_str_eq(tok_2, tok_1);
+        tok_1 = strtok(NULL, " !:");
+        tok_2 = my_strtok(NULL, " !:");
     }
-    cr_assert_null(str_1);
-    cr_assert_null(str_2);
+    cr_assert_null(tok_1);
+    cr_assert_null(tok_2);
 }
 
 Test(my_strtok, string_with_diff_mult_delim)
 {
     char str[] = "Hello ! :! Coucou!  ::!: :!!!Test! :! :! :: Epitech";
-    char *str_1 = strtok(str, " !:");
-    char *str_2 = my_strtok(str, " !:");
+    char *tok_1 = strtok(str, " !:");
+    char *tok_2 = my_strtok(str, " !:");
 
-     while (str_1 && str_2) {
-        cr_assert_str_eq(str_2, str_1);
-        str_1 = strtok(NULL, " !:");
-        str_2 = my_strtok(NULL, " !:");
+     while (tok_1 && tok_2) {
+        cr_assert_str_eq(tok_2, tok_1);
+        tok_1 = strtok(NULL, " !:");
+        tok_2 = my_strtok(NULL, " !:");
     }
-    cr_assert_null(str_1);
-    cr_assert_null(str_2);
+    cr_assert_null(tok_1);
+    cr_assert_null(tok_2);
 }
 
 Test(my_strtok, null_str)
